008ca9abd98cf53bb051defc5f8b8cc848d74ace
Johnstake1 <jonathansvetskin@gmail.com>
2024-11-24 00:19:54 +0200

    generates trxs, has issues with customer id

--- Modules/Transactions.py
+++ Modules/Transactions.py
@@ -1,7 +1,7 @@
+import datetime
 from faker import Faker
-from customer import Customer
 import random
-from db_connection import DatabaseConnection
+from Modules.Customer import UserRegistration
 
 class Transaction:
     def __init__(self, transaction_id, customer_id, transaction_date, transaction_amount, rounded_amount, category, merchant_name):
@@ -26,33 +26,33 @@ class Transaction:
             "merchant_name": self.merchant_name
         }
 
+    @staticmethod
+    def fake_transactions(db, customers, num_transactions=10000):
+        fake = Faker()
 
-def populate_fake_data(db, num_transactions=10000):
-    fake = Faker()
+        for i in range(1, num_transactions + 1):
+            customer = random.choice(customers)
+            customer_id = customer.customer_id
+            registration_date = customer.registration_date
+            transaction_date = fake.date_time_between(start_date=customer.registration_date, end_date="now")
+            transaction_amount = round(random.uniform(1.0, 10000.0), 2)
+            rounded_amount = int(transaction_amount) + (1 if transaction_amount != int(transaction_amount) else 0)
+            transaction = Transaction(
+                transaction_id=i,
+                customer_id=customer_id,
+                transaction_date=transaction_date,
+                transaction_amount=transaction_amount,
+                rounded_amount=rounded_amount,
+                category=fake.word(ext_word_list=["Groceries", "Electronics", "Clothing", "Utilities", "Dining"]),
+                merchant_name=fake.company()
+            )
 
-    # Generate fake transactions and insert them into the database
-    for i in range(1, num_transactions + 1):
-        customer_id = customer_id
-        registration_date = customers[customer_id - 1].registration_date
-        transaction_date = fake.date_time_between(start_date=registration_date, end_date="now")
-        transaction_amount = round(random.uniform(1.0, 1000.0), 2)
-        rounded_amount = int(transaction_amount) + (1 if transaction_amount != int(transaction_amount) else 0)
-        transaction = Transaction(
-            transaction_id=i,
-            customer_id=customer_id,
-            transaction_date=transaction_date,
-            transaction_amount=transaction_amount,
-            rounded_amount=rounded_amount,
-            category=fake.word(ext_word_list=["Groceries", "Electronics", "Clothing", "Utilities", "Dining"]),
-            merchant_name=fake.company()
-        )
-
-        insert_transaction_query = """
-        INSERT INTO Transaction (transaction_id, customer_id, transaction_date, transaction_amount, rounded_amount, unrounded_difference, category, merchant_name)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        db.execute_query(insert_transaction_query, (
-            transaction.transaction_id, transaction.customer_id, transaction.transaction_date,
-            transaction.transaction_amount, transaction.rounded_amount, transaction.unrounded_difference,
-            transaction.category, transaction.merchant_name
-        ))
+            insert_transaction_query = """
+            INSERT INTO Transaction (transaction_id, customer_id, transaction_date, transaction_amount, rounded_amount, unrounded_difference, category, merchant_name)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            """
+            db.execute_query(insert_transaction_query, (
+                transaction.transaction_id, transaction.customer_id, transaction.transaction_date,
+                transaction.transaction_amount, transaction.rounded_amount, transaction.unrounded_difference,
+                transaction.category, transaction.merchant_name
+            ))
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from Modules.db_connection import DatabaseConnection
 from Data.create_tables import create_tables
 from Utils.config import get_database_config
+from Modules.Customer import UserRegistration
+from Modules.Transactions import Transaction
 
 def main():
     db_config = get_database_config()
     db = DatabaseConnection(**db_config)
     db.connect()
     create_tables(db)
-    #need to add remaining processes
+    customers = [UserRegistration.input_customer_data()]
+    Transaction.fake_transactions(db, customers) 
     db.close()
 
 if __name__ == "__main__":
